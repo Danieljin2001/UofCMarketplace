5cbe5312b4c084ff0ee00189df49f81d79b61f5b
ArmaanMohar <armaan.mohar@gmail.com>
2022-12-08 16:50:12 -0700

    dynamic my products page, done all dynamic pages

--- backend/routes/student.js
+++ backend/routes/student.js
@@ -1,4 +1,4 @@
-import { createNewPost } from "../controllers/post";
+import { createNewPost, getMyPosts } from "../controllers/post";
 import { studentLogin, studentSignup } from "../controllers/student";
 import { verifyToken } from "../utils/verifyToken";
 
@@ -8,4 +8,6 @@ export const studentRouter = express.Router();
 studentRouter.post("/signup", studentSignup);
 studentRouter.post("/login", studentLogin);
 studentRouter.post("/createpost", verifyToken, createNewPost);
+studentRouter.post("/posts", verifyToken, getMyPosts);
+
 // studentRouter.post("/updatepost", verifyToken, updatePost);
